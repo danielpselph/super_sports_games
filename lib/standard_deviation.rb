6d61c4470ca08ecd92f5abb64d6b22aa7effcead
Daniel Selph <daniel.p.selph@gmail.com>
2019-11-21 13:39:48 -0700

    Completed step 3 of iteration 1

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,10 +1,24 @@
-ages = [24, 30, 18, 20, 41]
+def sum_of_array
+  ages = [24, 30, 18, 20, 41]
+  sum = ages.sum
+  p sum
+end
 
-sum = ages.sum
-
-p sum
+sum_of_array
 
 def integer_count
   ages = [24, 30, 18, 20, 41]
-  ages.find_all do |age|
+  new = ages.find_all do |age|
     (Integer)
+  end
+  p new.count
+end
+
+integer_count
+
+def divide_array_info
+  total = sum_of_array.to_f / integer_count.to_f
+  p total.to_f
+end
+
+divide_array_info
